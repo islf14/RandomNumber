7ed95f15fb5cc62642705d2e3f4488bc702e434e
islf14 <isaiaslflores@gmail.com>
2018-10-26 02:34:38 -0500

    todos metodos

--- multiplicador_constante.php
+++ multiplicador_constante.php
@@ -1,11 +1,13 @@
 <?php
     $D = 4;
     if(isset($_POST['enviar'])){
-        $x0 = $_POST['xi'];
+        $x0 = $_POST['x0'];
+        $a = $_POST['a'];
         $f = $_POST['f'];
         $D = $_POST['D'];
         $xi = $x0;
-        $long = strlen($x0);
+        $long_x0 = strlen($x0);
+        $long_a = strlen($a);
         $i = 0;
         $xi_2 = 0;
         $r = 0;
@@ -16,43 +18,48 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Algoritmo de cuadrados medios</title>
+    <title>Algoritmo de multiplicador constante</title>
     <link rel="stylesheet" href="estilo.css" >
 </head>
 <body>
     <main>
         <div class="contenedor-form">
-            <form class="formulario" action="cuadrados_medios.php" method="post">
-                <h3>Algoritmo de cuadrados medios</h3>
+            <form class="formulario" action="multiplicador_constante.php" method="post">
+                <h3>Algoritmo de multiplicador constante</h3>
                 <h4></h4>
-                <label for="xi">X0: </label>
-                <input class="input" type="number" name="xi" value = "<?php if(isset($_POST['enviar'])){echo $x0;}?>"><br>
+                <label for="x0">X0: </label>
+                <input class="input" type="number" name="x0" value = "<?php if(isset($_POST['enviar'])){echo $x0;}?>">
+                <label for="a">Constante a: </label>
+                <input class="input" type="number" name="a" value = "<?php if(isset($_POST['enviar'])){echo $a;}?>"><br>
                 <label for="D">D: </label>
                 <input class="input" type="number" name="D" value = "<?php echo $D;?>"><br>
-                <label for="#">Filas a generar</label>
+                <label for="#">Ingrese filas a generar</label>
                 <input class="input" type="number" name="f" value = "<?php if(isset($_POST['enviar'])){echo $f;}?>"><br>
                 <button class="boton btn-primary" name="enviar">calcular</button>
             </form>
             <div class="resultado">
                 <h3>Calculados:</h3>
-                <p>X0: <?php if(isset($_POST['enviar'])){ echo $x0;}?></p>
+                <label>X0: <?php if(isset($_POST['enviar'])){ echo $x0;}?></label><br>
+                <label>a: <?php if(isset($_POST['enviar'])){ echo $a;}?></label>
                 <p>Filas: <?php if(isset($_POST['enviar'])){ echo $f;}?></p>
-                <p>Longitud X0: <?php if(isset($_POST['enviar'])){ echo $long;}?></p>
+                <label>Longitud X0: <?php if(isset($_POST['enviar'])){ echo $long_x0;}?></label><br>
+                <label>Longitud a: <?php if(isset($_POST['enviar'])){ echo $long_a;}?></label>
             </div>
         </div>
         <table class="tabla">
             <tr class="titulo_tabla">
                 <th class = "width_10">i</th>
+                <th class = "width_10">a</th>
                 <th class = "width_10">Xi</th>
-                <th class = "width_20">Xi^2</th>
-                <th class = "width_20">long Xi^2</th>
+                <th class = "width_20">(a)*(Xi)</th>
+                <th class = "width_10">Long</th>
                 <th class = "width_10">Medios</th>
                 <th>ri</th>
             </tr>
             <?php
                 if(isset($_POST['enviar'])){
                     for($i=0;$i<$f;$i++){
-                        $xi_2 = pow($xi,2);//al cuadrado
+                        $xi_2 = $xi*$a;//multiplicacion
                         $l_xi_2 = strlen($xi_2);//su longitud
                         $array_xi_2 = str_split($xi_2);
                         $resta = $l_xi_2 - $D;/// 9 - 5 = 4
@@ -85,6 +92,7 @@
                         $r = '0.'.$newString;
                         echo "<tr class='fila_tabla'>
                                 <td>".$i."</td>
+                                <td>".$a."</td>
                                 <td>".$xi."</td>
                                 <td>".$xi_2."</td>
                                 <td>".$l_xi_2."</td>
--- productos_medios.php
+++ productos_medios.php
@@ -1,11 +1,14 @@
 <?php
     $D = 4;
     if(isset($_POST['enviar'])){
-        $x0 = $_POST['xi'];
+        $x0 = $_POST['x0'];
+        $x1 = $_POST['x1'];
         $f = $_POST['f'];
         $D = $_POST['D'];
         $xi = $x0;
-        $long = strlen($x0);
+        $xii = $x1;
+        $long_x0 = strlen($x0);
+        $long_x1 = strlen($x1);
         $i = 0;
         $xi_2 = 0;
         $r = 0;
@@ -16,43 +19,48 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Algoritmo de cuadrados medios</title>
+    <title>Algoritmo de productos medios</title>
     <link rel="stylesheet" href="estilo.css" >
 </head>
 <body>
     <main>
         <div class="contenedor-form">
-            <form class="formulario" action="cuadrados_medios.php" method="post">
-                <h3>Algoritmo de cuadrados medios</h3>
+            <form class="formulario" action="productos_medios.php" method="post">
+                <h3>Algoritmo de productos medios</h3>
                 <h4></h4>
                 <label for="xi">X0: </label>
-                <input class="input" type="number" name="xi" value = "<?php if(isset($_POST['enviar'])){echo $x0;}?>"><br>
+                <input class="input" type="number" name="x0" value = "<?php if(isset($_POST['enviar'])){echo $x0;}?>">
+                <label for="xi">X1: </label>
+                <input class="input" type="number" name="x1" value = "<?php if(isset($_POST['enviar'])){echo $x1;}?>"><br>
                 <label for="D">D: </label>
                 <input class="input" type="number" name="D" value = "<?php echo $D;?>"><br>
-                <label for="#">Filas a generar</label>
+                <label for="#">Ingrese filas a generar</label>
                 <input class="input" type="number" name="f" value = "<?php if(isset($_POST['enviar'])){echo $f;}?>"><br>
                 <button class="boton btn-primary" name="enviar">calcular</button>
             </form>
             <div class="resultado">
                 <h3>Calculados:</h3>
-                <p>X0: <?php if(isset($_POST['enviar'])){ echo $x0;}?></p>
+                <label>X0: <?php if(isset($_POST['enviar'])){ echo $x0;}?></label><br>
+                <label>X1: <?php if(isset($_POST['enviar'])){ echo $x1;}?></label>
                 <p>Filas: <?php if(isset($_POST['enviar'])){ echo $f;}?></p>
-                <p>Longitud X0: <?php if(isset($_POST['enviar'])){ echo $long;}?></p>
+                <label>Longitud X0: <?php if(isset($_POST['enviar'])){ echo $long_x0;}?></label><br>
+                <label>Longitud X1: <?php if(isset($_POST['enviar'])){ echo $long_x1;}?></label>
             </div>
         </div>
         <table class="tabla">
             <tr class="titulo_tabla">
                 <th class = "width_10">i</th>
                 <th class = "width_10">Xi</th>
-                <th class = "width_20">Xi^2</th>
-                <th class = "width_20">long Xi^2</th>
+                <th class = "width_10">Xi+1</th>
+                <th class = "width_20">(Xi)*(Xi+1)</th>
+                <th class = "width_10">Long</th>
                 <th class = "width_10">Medios</th>
                 <th>ri</th>
             </tr>
             <?php
                 if(isset($_POST['enviar'])){
                     for($i=0;$i<$f;$i++){
-                        $xi_2 = pow($xi,2);//al cuadrado
+                        $xi_2 = $xi*$xii;//multiplicacion
                         $l_xi_2 = strlen($xi_2);//su longitud
                         $array_xi_2 = str_split($xi_2);
                         $resta = $l_xi_2 - $D;/// 9 - 5 = 4
@@ -86,12 +94,14 @@
                         echo "<tr class='fila_tabla'>
                                 <td>".$i."</td>
                                 <td>".$xi."</td>
+                                <td>".$xii."</td>
                                 <td>".$xi_2."</td>
                                 <td>".$l_xi_2."</td>
                                 <td>".$newString."</td>
                                 <td>".$r."</td>
                             </tr>";
-                        $xi = $newString;
+                        $xi=$xii;
+                        $xii = $newString;
                     }
                 }
             ?>
